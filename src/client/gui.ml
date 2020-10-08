b9ee33a8a0929c55ed39ca142addf629f4b51cb3
Sebastien Mondet <seb@mondet.org>
2020-10-08 19:10:12 -0400

    Start properly building the `Gui` module

--- src/client/gui.ml
+++ src/client/gui.ml
@@ -2,21 +2,84 @@ open Import
 
 module State = struct
   module Page = struct
-    type t = Welcome | Settings
+    type t = Explorer | Settings | About
 
-    let to_string = function Welcome -> "Welcome" | Settings -> "Settings"
+    let to_string = function
+      | Explorer -> "Explorer"
+      | Settings -> "Settings"
+      | About -> "About"
   end
 
   open Page
 
-  type t = {page: Page.t Lwd.var}
+  type t =
+    {page: Page.t Lwd.var; version_string: string option; dev_mode: bool Lwd.var}
 
-  let create () = {page= Lwd.var Welcome}
+  let create () =
+    {page= Lwd.var Explorer; version_string= None; dev_mode= Lwd.var true}
 end
 
+let navigation_menu state =
+  let open State in
+  let open Page in
+  let open Meta_html in
+  let current_is_not p = Lwd.get state.page |> Lwd.map Poly.(( <> ) p) in
+  let act p () = Lwd.set state.page p in
+  Bootstrap.Navigation_bar.(
+    make
+      ~brand:(Bootstrap.label `Dark (t "TZComet"))
+      (let of_page p =
+         let fragment = Page.to_string p in
+         item (bt fragment) ~active:(current_is_not p) ~action:(act p) ~fragment
+       in
+       List.map ~f:of_page [Explorer; Settings; About]))
+
+let about_page state =
+  let open Meta_html in
+  let open State in
+  let p = p_lead in
+  h2 (t "TZComet")
+  % p
+      ( t "This is"
+      %% link ~target:"https://github.com/tqtezos/TZComet" (t "TZComet")
+      %% ( match state.version_string with
+         | None -> it "unknown version"
+         | Some vs ->
+             t "version "
+             %% link
+                  ~target:
+                    (Fmt.str "https://github.com/tqtezos/TZComet/commit/%s" vs)
+                  (it vs) )
+      % bind_var state.State.dev_mode ~f:(function
+          | true -> t " (in “dev” mode)."
+          | false -> t ".") )
+  % p (t "An explorer/editor/validator/visualizer for Tezos contract metadata.")
+  % p
+      ( t "The source for this webpage is available on Github:"
+      %% link ~target:"https://github.com/tqtezos/TZComet"
+           (ct "tqtezos/TZComet")
+      % t "." )
+  % p
+      ( t "The Contract Metadata standard, a.k.a. TZIP-16, is at: "
+      % url ct
+          "https://gitlab.com/tzip/tzip/-/blob/master/proposals/tzip-16/tzip-16.md"
+      % t "." )
+
 let root_document () =
-  let open Tyxml_lwd in
+  let open Meta_html in
   let state = State.create () in
+  Bootstrap.container ~suffix:"-fluid"
+    ( navigation_menu state
+    % bind_var state.page
+        ~f:
+          State.Page.(
+            function
+            | Explorer -> t "Welcome/explorer page TODO"
+            | Settings -> t "Settings page"
+            | About -> about_page state) )
+
+(*
+let open Tyxml_lwd in
   Html.div
     [ Meta_html.Example.e1 ()
     ; Html.p
@@ -32,3 +95,5 @@ let root_document () =
                        true)) ]
             [Html.txt (Lwd.get state.State.page |> Lwd.map State.Page.to_string)]
         ] ]
+
+      *)
