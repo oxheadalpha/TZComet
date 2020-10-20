8d8ff81d662b8acf9873b181d96f2d12c5021ea1
Sebastien Mondet <seb@mondet.org>
2020-10-20 13:01:58 -0400

    Add “Event more weird off-chain-views” dev-example

--- src/client/gui.ml
+++ src/client/gui.ml
@@ -236,6 +236,8 @@ module State = struct
              "Has a URI that points nowhere." ;
            kt1 "KT1AtHTLvsBVy2yGPw9LWGMnhG2vL5ucm7ak"
              "Quite a few off-chain-view tests." ;
+           kt1_dev "KT1XNWz276oab4SBVcYea6hP49kgvc8xXqrK"
+             "Event more weird off-chain-views." ;
            uri https_ok "A valid HTTPS URI." ;
            uri sha256_https_ok "A valid SHA256+HTTPS URI." ;
            uri_dev sha256_https_ko
