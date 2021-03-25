85d79b6667d4c0be8b0f63402016b098868b738e
Sebastien Mondet <seb@mondet.org>
2021-03-24 20:35:41 -0400

    Tweak candidate probabilities

--- src/client/state.ml
+++ src/client/state.ml
@@ -446,9 +446,9 @@ module Examples = struct
 
   let tokens_global =
     (* weight, name, kt1, min-token, max-token *)
-    [ (0.05, "Alchememist", "KT1W4wh1qDc2g22DToaTfnCtALLJ7jHn38Xc", 0, 15)
-    ; (0.85, "HicEtNunc", "KT1RJ6PbjHpwc3M5rw5s2Nbmefwbuwbdxton", 300, 12167)
-    ; (0.1, "Kalamint", "KT1EpGgjQs73QfFJs9z7m1Mxm5MTnpC2tqse", 1, 254) ]
+    [ (0.1, "Alchememist", "KT1W4wh1qDc2g22DToaTfnCtALLJ7jHn38Xc", 0, 15)
+    ; (0.7, "HicEtNunc", "KT1RJ6PbjHpwc3M5rw5s2Nbmefwbuwbdxton", 300, 12167)
+    ; (0.2, "Kalamint", "KT1EpGgjQs73QfFJs9z7m1Mxm5MTnpC2tqse", 1, 254) ]
 
   let random_token (_ : _ Context.t) =
     let _, _, k, m, x =
