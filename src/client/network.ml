281027f2b398a1b72c251b3795a8acd11821180a
Sebastien Mondet <seb@mondet.org>
2022-01-28 13:16:13 -0500

    Remvoe Idiazabalnet and update nodes

--- src/client/network.ml
+++ src/client/network.ml
@@ -1,13 +1,11 @@
 open! Import
 
-type t =
-  [`Mainnet | `Hangzhounet | `Granadanet | `Idiazabalnet | `Ithacanet | `Sandbox]
+type t = [`Mainnet | `Hangzhounet | `Granadanet | `Ithacanet | `Sandbox]
 
 let to_string : t -> string = function
   | `Mainnet -> "Mainnet"
   | `Granadanet -> "Granadanet"
   | `Hangzhounet -> "Hangzhounet"
-  | `Idiazabalnet -> "Idiazabalnet"
   | `Ithacanet -> "Ithacanet"
   | `Sandbox -> "Sandbox"
 
@@ -15,9 +13,7 @@ let better_call_dev_path : t -> string option = function
   | `Mainnet -> Some "mainnet"
   | `Granadanet -> Some "granadanet"
   | `Hangzhounet -> Some "hangzhounet"
-  | `Idiazabalnet -> Some "idiazabalnet"
   | `Ithacanet -> Some "ithacanet"
   | `Sandbox -> None
 
-let all : t list =
-  [`Mainnet; `Granadanet; `Hangzhounet; `Idiazabalnet; `Ithacanet; `Sandbox]
+let all : t list = [`Mainnet; `Granadanet; `Hangzhounet; `Ithacanet; `Sandbox]
