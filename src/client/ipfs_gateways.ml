fee49da444184eff501826fae6f015350d8284c8
Mark Nichols <marklnichols@gmail.com>
2022-03-18 14:08:44 -0400

    Added get_nth for multiple uses of nth_exn

--- src/client/ipfs_gateways.ml
+++ src/client/ipfs_gateways.ml
@@ -14,12 +14,22 @@ let get (ctxt : < ipfs_gateways: t ; .. > Context.t) = ctxt#ipfs_gateways
 let gateways t = (get t).gateways
 let current_index t = (get t).current_index
 
+exception Bad_gateway_index of string
+
+let get_nth the_list idx =
+  let count = List.length the_list in
+  let error_str =
+    Printf.sprintf "Trying to use index %d on a list of gateways of size %d" idx
+      count in
+  let new_gw =
+    try List.nth_exn the_list idx
+    with Invalid_argument _ -> raise (Bad_gateway_index error_str) in
+  new_gw
+
 let current_gateway t =
   let the_list = Reactive.peek (get t).gateways in
   let idx = Reactive.peek (get t).current_index in
-  List.nth_exn the_list idx
-
-exception Bad_gateway_index of string
+  get_nth the_list idx
 
 let try_next ctxt =
   let ipfs = get ctxt in
@@ -29,12 +39,7 @@ let try_next ctxt =
   let count = List.length the_list in
   let new_idx = if phys_equal idx (count - 1) then 0 else idx + 1 in
   Reactive.set (current_index ctxt) new_idx ;
-  let error_str =
-    Printf.sprintf "Trying to use index %d on a list of gateways of size %d" idx
-      count in
-  let new_gw =
-    try List.nth_exn the_list new_idx
-    with Invalid_argument _ -> raise (Bad_gateway_index error_str) in
+  let new_gw = get_nth the_list new_idx in
   dbgf "Ipfs_gateways.try_next - rotating IPFS gateways: %S ----> %S" old_gw
     new_gw ;
   new_gw
